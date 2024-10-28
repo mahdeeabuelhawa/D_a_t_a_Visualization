ff4fff8511d863d4181172375704b88387c1030c
Mahdee Abuelhawa <mahdeeabuelhawa@yahoo.com>
2024-10-28 14:50:23 -0400

    Add files via upload

---
+++ dye.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from die import Die
+
+# Create a D6.
+die = Die()
+
+# Make some rolls, and store results in a list.
+results = []
+for roll_num in range(100):
+    result = die.roll()
+    results.append(result)
+
+print(results)
+
+
+# In[3]:
+
+
+from die import Die
+
+# Create a D6.
+die = Die()
+
+# Make some rolls, and store results in a list.
+results = []
+for roll_num in range(1000):
+    result = die.roll()
+    results.append(result)
+
+# Analyze the results.
+frequencies = []
+poss_results = range(1, die.num_sides+1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
+
+
+# In[4]:
+
+
+import plotly.express as px
+
+from die import Die
+
+
+# Create a D6.
+die = Die()
+
+# Make some rolls, and store results in a list.
+results = []
+for roll_num in range(1000):
+    result = die.roll()
+    results.append(result)
+
+# Analyze the results.
+frequencies = []
+poss_results = range(1, die.num_sides+1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize the results.
+fig = px.bar(x=poss_results, y=frequencies)
+fig.show()
+
+
+# In[8]:
+
+
+import plotly.express as px
+
+from die import Die
+
+
+# Create two D6 dice.
+die_1 = Die()
+die_2 = Die()
+
+# Make some rolls, and store results in a list.
+results = []
+for roll_num in range(1000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
+
+# Analyze the results.
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_result+1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize the results.
+title = "Results of Rolling Two D6 Dice 1,000 Times"
+labels = {'x': 'Result', 'y': 'Frequency of Result'}
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+
+# Further customize chart.
+fig.update_layout(xaxis_dtick=1)
+              
+fig.show()
+
+
+# In[5]:
+
+
+import plotly.express as px
+
+from die import Die
+
+
+# Create two D6 dice.
+die_1 = Die()
+die_2 = Die()
+
+# Make some rolls, and store results in a list.
+results = []
+for roll_num in range(1000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
+
+# Analyze the results.
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_result+1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize the results.
+title = "Results of Rolling Two D6 Dice 1,000 Times"
+labels = {'x': 'Result', 'y': 'Frequency of Result'}
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+
+# Further customize chart.
+fig.update_layout(xaxis_dtick=1)
+              
+fig.show()
+
+
+# In[6]:
+
+
+import plotly.express as px
+
+from die import Die
+
+
+# Create a D6.
+die = Die()
+
+# Make some rolls, and store results in a list.
+results = []
+for roll_num in range(1000):
+    result = die.roll()
+    results.append(result)
+
+# Analyze the results.
+frequencies = []
+poss_results = range(1, die.num_sides+1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize the results.
+title = "Results of Rolling One D6 1,000 Times"
+labels = {'x': 'Result', 'y': 'Frequency of Result'}
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+fig.show()
+
+
+# In[7]:
+
+
+import plotly.express as px
+
+from die import Die
+
+
+# Create a D6 and a D10.
+die_1 = Die()
+die_2 = Die(10)
+
+# Make some rolls, and store results in a list.
+results = []
+for roll_num in range(50_000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
+
+# Analyze the results.
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_result+1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize the results.
+title = "Results of Rolling a D6 and a D10 50,000 Times"
+labels = {'x': 'Result', 'y': 'Frequency of Result'}
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+
+# Further customize chart.
+fig.update_layout(xaxis_dtick=1)
+              
+fig.show()
+
+
+# In[ ]:
+
+
+
+
